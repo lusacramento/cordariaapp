8799f7111d72f47102491a78f1b73ca6f3b0ff9e
lusacramento <lucianossacramento@yahoo.com.br>
2023-08-29 11:47:14 -0300

    redirect base git pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   target: 'static',
   router: {
-    // base: '/Cordaria/',
+    base: '/Cordaria/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
