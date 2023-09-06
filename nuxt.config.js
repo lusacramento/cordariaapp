ec320a48547f3135b9e9d454810232783b451f78
lusacramento <lucianossacramento@yahoo.com.br>
2023-09-06 12:14:06 -0300

    fix deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   target: 'static',
   router: {
-    base: '/Cordaria/',
+    base: '/cordariaapp/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
